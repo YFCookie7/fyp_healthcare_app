e3dece54098abfc17bbc2aa2bf116f98d4035270
YFCookie7 <mikeli1117@gmail.com>
2024-03-10 23:07:46 +0800

    get pi local ip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fyp_healthcare_app/splash_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:developer' as developer;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  String piAddress = await getPiAddress('raspberry');
+  developer.log('Raspberry Pi IP Address: $piAddress', name: 'debug');
+
+  String storedPiAddress = prefs.getString('piAddress') ?? '';
+  if (storedPiAddress != piAddress && piAddress != '') {
+    prefs.setString('piAddress', piAddress);
+    developer.log('New ip address record stored in shared preference',
+        name: 'debug');
+  }
+
   runApp(const MainApp());
 }
 
@@ -12,4 +29,18 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(home: SplashScreen());
   }
+}
+
+Future<String> getPiAddress(String hostname) async {
+  try {
+    final result = await InternetAddress.lookup(hostname);
+    if (result.isNotEmpty && result.first.rawAddress.isNotEmpty) {
+      return result.first.address;
+    } else {
+      developer.log('Failed to find the device', name: 'debug');
+    }
+  } on SocketException catch (e) {
+    developer.log('Error: $e', name: 'debug');
+  }
+  return '';
 }
