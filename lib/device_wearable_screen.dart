1ea4d1fc6c63851cecc9366554f867cfc2413642
YFCookie7 <mikeli1117@gmail.com>
2024-04-15 15:21:34 +0800

    heartbeat and body temp

--- lib/device_wearable_screen.dart
+++ lib/device_wearable_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:syncfusion_flutter_gauges/gauges.dart';
 import 'package:fyp_healthcare_app/data-comm/ble.dart';
 import 'dart:math';
@@ -18,6 +19,8 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
   String receivedData = '';
   String textbox = 'Hi';
   double tempValue = 37.0;
+  int heartbeatValue = 60;
+  double heartbeatValue_double = 0.0;
 
   String titleText = "Connecting to device";
   String redText = "redText";
@@ -60,6 +63,9 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
 
   void _handleDataReceived(String data) {
     setState(() {
+      textbox = "$data ${Random().nextInt(1000)}";
+      developer.log(data, name: 'debug.device_watch');
+
       // List<int> datum = data.codeUnits;
       // var value = datum[1] * 256 + datum[0];
       // redText = "$value";
@@ -75,10 +81,11 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
       // gyroYText = "$value";
       // value = datum[13] * 256 + datum[12];
       // gyroZText = "$value";
-      textbox = "$data ${Random().nextInt(1000)}";
-      developer.log(data, name: 'debug.device_watch');
       tempValue = 34 + Random().nextDouble() * (40 - 34);
-      //BluetoothBLE.sendMessage("OK\n");
+      heartbeatValue = 60 + Random().nextInt(40);
+      heartbeatValue_double = ((heartbeatValue) * (90) / (160));
+      developer.log(heartbeatValue_double.toString(),
+          name: 'debug.device_watch');
     });
   }
 
@@ -88,32 +95,109 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
       appBar: AppBar(
         title: Text(titleText),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              textbox,
-              style: const TextStyle(fontSize: 20),
-            ),
-            SfRadialGauge(axes: <RadialAxis>[
-              RadialAxis(minimum: 34, maximum: 40, ranges: <GaugeRange>[
-                GaugeRange(startValue: 34, endValue: 36, color: Colors.green),
-                GaugeRange(startValue: 36, endValue: 38, color: Colors.orange),
-                GaugeRange(startValue: 38, endValue: 40, color: Colors.red)
-              ], pointers: <GaugePointer>[
-                NeedlePointer(value: tempValue, enableAnimation: true)
-              ], annotations: <GaugeAnnotation>[
-                GaugeAnnotation(
-                    widget: Container(
-                        child: Text(tempValue.toStringAsFixed(1),
-                            style: TextStyle(
-                                fontSize: 25, fontWeight: FontWeight.bold))),
-                    angle: 90,
-                    positionFactor: 0.5)
-              ])
-            ])
-          ],
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                textbox,
+                style: const TextStyle(fontSize: 20),
+              ),
+              SizedBox(
+                width: 250,
+                height: 250,
+                child: SfRadialGauge(axes: <RadialAxis>[
+                  RadialAxis(minimum: 34, maximum: 40, ranges: <GaugeRange>[
+                    GaugeRange(
+                        startValue: 34, endValue: 36, color: Colors.green),
+                    GaugeRange(
+                        startValue: 36, endValue: 38, color: Colors.orange),
+                    GaugeRange(startValue: 38, endValue: 40, color: Colors.red)
+                  ], pointers: <GaugePointer>[
+                    NeedlePointer(value: tempValue, enableAnimation: true)
+                  ], annotations: <GaugeAnnotation>[
+                    GaugeAnnotation(
+                        widget: Container(
+                          width: 140.00,
+                          height: 50.00,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              const Image(
+                                image: ExactAssetImage(
+                                    'assets/icon/body_temperature.png'),
+                                fit: BoxFit.fitHeight,
+                                height: 35,
+                              ),
+                              const SizedBox(width: 4),
+                              Text(
+                                "${tempValue.toStringAsFixed(1)}°C",
+                                style: const TextStyle(
+                                    fontSize: 25, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                        angle: 90,
+                        positionFactor: 0.5)
+                  ]),
+                ]),
+              ),
+              SizedBox(
+                width: 250,
+                height: 250,
+                child: SfRadialGauge(axes: <RadialAxis>[
+                  RadialAxis(
+                      interval: 10,
+                      startAngle: 270,
+                      endAngle: 270,
+                      showTicks: false,
+                      showLabels: false,
+                      axisLineStyle: AxisLineStyle(thickness: 20),
+                      pointers: <GaugePointer>[
+                        RangePointer(
+                            value: heartbeatValue_double,
+                            width: 20,
+                            color: Color(0xFFFFCD60),
+                            enableAnimation: true,
+                            cornerStyle: CornerStyle.bothCurve)
+                      ],
+                      annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                            widget: Column(
+                              children: <Widget>[
+                                Container(
+                                  width: 200.00,
+                                  height: 80.00,
+                                  child: Row(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      Lottie.asset(
+                                        'assets/lottie/heartbeat_lottie.json',
+                                        width: 90,
+                                        height: 90,
+                                        // fit: BoxFit.cover,
+                                      ),
+                                      const SizedBox(width: 2),
+                                      Text(
+                                        '${heartbeatValue.toStringAsFixed(0)} bpm',
+                                        style: const TextStyle(
+                                            fontSize: 25,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                            angle: 270,
+                            positionFactor: 0)
+                      ])
+                ]),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: SplashScreen());
+        debugShowCheckedModeBanner: false, home: WearableDeviceScreen());
   }
 }
 
