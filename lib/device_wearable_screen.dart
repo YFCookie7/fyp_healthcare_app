a45d74c980bbec4b560977ee32de91f270bc7fb7
HKJackit <hk.jackit@gmail.com>
2024-04-17 02:35:29 +0800

    spo2 and temperature value
    + spo2 calculation
    + object temperature value display
    + dispose of corrupted data frame

--- lib/device_wearable_screen.dart
+++ lib/device_wearable_screen.dart
@@ -21,10 +21,41 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
   String receivedData = '';
   String textbox = 'Hi';
   double tempValue = 37.0;
-  int heartbeatValue = 60;
+  double heartbeatValue = 60;
   double heartbeatValue_double = 0.0;
 
   String titleText = "Connecting to device";
+
+  double spo2 = 0;
+  int nowRed = 0;
+  int lastRed = 0;
+  int nowIr = 0;
+  int lastIr = 0;
+  int nowTempA = 0;
+  int lastTempA = 0;
+  int nowTempO = 0;
+  int lastTempO = 0;
+  int nowGX = 0;
+  int lastGX = 0;
+  int nowGY = 0;
+  int lastGY = 0;
+  int nowGZ = 0;
+  int lastGZ = 0;
+
+  int redNowMax = 65535;
+  int irNowMax = 65535;
+  int redNextMax = 0;
+  int irNextMax = 0;
+
+  int redNowMin = 0;
+  int irNowMin = 0;
+  int redNextMin = 65535;
+  int irNextMin = 65535;
+
+  int nowSecond = 0;
+  int lastSecond = 0;
+
+  /*
   String redText = "redText";
   String irText = "irText";
   String tempAText = "tempAText";
@@ -32,6 +63,7 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
   String gyroXText = "gyroXText";
   String gyroYText = "gyroYText";
   String gyroZText = "gyroZText";
+  */
 
   @override
   void initState() {
@@ -68,29 +100,89 @@ class _WearableDeviceScreenState extends State<WearableDeviceScreen> {
       textbox = "$data ${Random().nextInt(1000)}";
       developer.log(data, name: 'debug.device_watch');
 
-      // List<int> datum = data.codeUnits;
-      // var value = datum[1] * 256 + datum[0];
-      // redText = "$value";
-      // value = datum[3] * 256 + datum[2];
-      // irText = "$value";
-      // value = datum[5] * 256 + datum[4];
-      // tempAText = "$value";
-      // value = datum[7] * 256 + datum[6];
-      // tempOText = "$value";
-      // value = datum[9] * 256 + datum[8];
-      // gyroXText = "$value";
-      // value = datum[11] * 256 + datum[10];
-      // gyroYText = "$value";
-      // value = datum[13] * 256 + datum[12];
-      // gyroZText = "$value";
+      /*
       tempValue = 34 +
           Random().nextDouble() *
               (40 - 34); // replace this with actual temperature
+       */
+
+      DateTime now = DateTime.now();
+      nowSecond = now.second;
+
+      if(nowSecond!=lastSecond)
+      {
+        redNowMax = redNextMax;
+        redNowMin = redNextMin;
+        redNextMax = 0;
+        redNextMin = 65535;
+
+        irNowMax = irNextMax;
+        irNowMin = irNextMin;
+        irNextMax = 0;
+        irNextMin = 65535;
+      }
+
+
+      if(data.length==14)
+      {
+        List<int> datum = data.codeUnits;
+        nowRed = datum[1]*256 + datum[0];
+        nowIr = datum[3]*256 + datum[2];
+        nowTempA = datum[5]*256 + datum[4];
+        nowTempO = datum[7]*256 + datum[6];
+        nowGX = datum[9]*256 + datum[8];
+        nowGY = datum[11]*256 + datum[10];
+        nowGZ = datum[13]*256 + datum[12];
+      }
+
+      if(nowIr>irNextMax)
+      {
+        irNextMax = nowIr;
+      }
+      if(nowIr<irNextMin)
+      {
+        irNextMin = nowIr;
+      }
+      if(nowRed>redNextMax)
+      {
+        redNextMax = nowRed;
+      }
+      if(nowRed<redNextMin)
+      {
+        redNextMin = nowRed;
+      }
+
+      tempValue = nowTempO/50 - 273.15;
+
+      if(nowIr>4096 && nowRed>4096)
+      {
+        spo2 = 110-(25*nowRed*redNowMin)/(nowIr*irNowMin);
+      }
+      else
+      {
+        spo2 = 0;
+      }
+
+      heartbeatValue = spo2;
+
+      //heartbeatValue = nowSecond;
+
+      /*
       heartbeatValue =
           60 + Random().nextInt(40); // replace this with actual heartbeat
       heartbeatValue_double = ((heartbeatValue) * (90) / (160));
       developer.log(heartbeatValue_double.toString(),
           name: 'debug.device_watch');
+      */
+
+      lastRed = nowRed;
+      lastIr = nowIr;
+      lastTempA = nowTempA;
+      lastTempO = nowTempO;
+      lastGX = nowGX;
+      lastGY = nowGY;
+      lastGZ = nowGZ;
+      lastSecond = nowSecond;
     });
   }
 
